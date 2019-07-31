c90a819e90dda588f9f77a10a779d034d7c06a20
Devan <dbg1709@gmail.com>
2019-07-31 16:07:29 +0200

    Added my enviroment testing scene

---
+++ Celeste Clone/Assets/Scripts/DevanScripts/JumpyPad.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpyPad : MonoBehaviour
+{
+    public float jumpForce = 12;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            GameObject.Find("Player").GetComponent<Rigidbody2D>().velocity += Vector2.up * jumpForce;
+        }
+    }
+
+}
