599992b956e5189d5cf44afef8bc6213191c3f25
Devan <dbg1709@gmail.com>
2019-08-01 00:01:51 +0200

    Camera movement (transitions) moving from level to level is working, Spikes and Jumpy Pad are still in progress.

---
+++ Celeste Clone/Assets/Scripts/DevanScripts/SpikeContact.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeContact : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name.Equals("Player"))
+        {
+            foreach(ContactPoint2D hitPos in collision.contacts)
+            {
+                if(hitPos.normal.y == -1)
+                {
+                    GameObject.Find("SpawnPoints").GetComponent<SpawnPoints>().RespawnPlayer();
+                }
+            }
+        }
+    }
+}
