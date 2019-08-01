4575c51f15b0bc8eebbee3a1146097a7256625d9
Keaton de Jager <keatondejager@gmail.com>
2019-08-01 09:17:38 +0200

    Rewrote movement code to be more agile.

---
+++ Celeste Clone/Assets/Scripts/Player/PlatformerMovement.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CollisionDetection))]
+public class PlatformerMovement : MonoBehaviour
+{
+    [Header("Component Reference")]
+        [HideInInspector] public Rigidbody2D rb;
+        [HideInInspector] public CollisionDetection coll;
+ 
+    //! [Header("Object Reference")]
+
+    [Header("Control Variables")]
+        public float moveSpeed = 5f; 
+        public float jumpForce = 3f;
+        public float fallMultiplier = 2.5f;
+        private bool wallGrab;
+    
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!rb) rb = GetComponent<Rigidbody2D>();
+        if (!coll) coll = GetComponent<CollisionDetection>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        wallGrab = coll.onWall && Input.GetButton("Grab");
+
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Move(x, y);
+
+        if (Input.GetButtonDown("Jump")) {
+            if (coll.grounded || coll.onWall)
+                Jump(x, y);
+        }
+
+        if (!coll.grounded && !Input.GetButton("Jump")) {
+            rb.velocity += (Physics2D.gravity * (fallMultiplier - 1) ) * Time.deltaTime;
+        }
+
+        if (wallGrab) {
+            rb.gravityScale = 0;
+        } else {
+            rb.gravityScale = 1;
+        }
+    }
+
+    private void Move (float x, float y) {
+        if (coll.grounded) {
+            rb.velocity = new Vector2 (x * moveSpeed, rb.velocity.y);
+        } 
+        if (wallGrab) {
+            rb.velocity = new Vector2 (rb.velocity.x, y * moveSpeed);
+        } else if (!coll.onWall) {
+            rb.velocity = new Vector2 (x * moveSpeed, rb.velocity.y);
+        }
+    
+    }
+
+    private void Jump (float x, float y) {
+        if (x == 0) {
+            rb.velocity = new Vector2 (rb.velocity.x, jumpForce);
+        } else if (coll.onWall) {
+            switch (coll.Wall) {
+                case -1: //Wall on the left of the character
+                    if (x < 0) {
+                        rb.velocity = new Vector2 (rb.velocity.x, jumpForce);
+                    } else {
+                        rb.velocity = new Vector2 (rb.velocity.x + x * jumpForce, jumpForce);
+                    }
+                    break;
+                case 1: //Wall on the right of the character
+                    if (x > 0) {
+                        rb.velocity = new Vector2 (rb.velocity.x, jumpForce);
+                    } else {
+                        rb.velocity = new Vector2 (rb.velocity.x + x * jumpForce, jumpForce);
+                    }
+                    break;
+            }
+        } else {
+            rb.velocity = new Vector2 (x * jumpForce, jumpForce);
+        }
+    }
+}
