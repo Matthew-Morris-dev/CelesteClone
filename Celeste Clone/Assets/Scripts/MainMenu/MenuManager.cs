d02e296f81b65964d18f0ddbbeb7e0bb0fab696f
Keaton de Jager <keatondejager@gmail.com>
2019-08-02 17:45:07 +0200

    A lot of work done on start menu

---
+++ Celeste Clone/Assets/Scripts/MainMenu/MenuManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    private AsyncOperation MenuScene;
+    private void Start() {
+        MenuScene = SceneManager.LoadSceneAsync(1, LoadSceneMode.Single);
+        MenuScene.allowSceneActivation = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump")) {
+            MenuScene.allowSceneActivation = true;
+        }   
+    }
+}
---
+++ Celeste Clone/Assets/Scripts/MainMenu/PauseMenu.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{   
+
+    [Header("Object Reference")]
+        public GameObject PauseScreen;
+        public PlatformerMovement playerMove;
+        public Rigidbody2D playerPhysics;
+        public Dash playerDash;
+
+        [Space]
+
+        public GameObject ConfirmMessage;
+
+    [Header("Values")]
+        private bool GamePaused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject Player = GameObject.FindWithTag("Player");
+        playerDash = Player.GetComponent<Dash>();
+        playerPhysics = Player.GetComponent<Rigidbody2D>();
+        playerMove = Player.GetComponent<PlatformerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PauseScreen.SetActive(GamePaused);
+        playerDash.enabled = !GamePaused;
+        playerPhysics.simulated = !GamePaused;
+        playerMove.enabled = !GamePaused;
+
+        if (Input.GetButtonDown("Pause")) {
+            GamePaused = !GamePaused;
+        }
+    }
+
+    public void ResumeGame () {
+        GamePaused = false;
+    }
+
+    public void RestartGame() {
+        // Respawn Player
+        GamePaused = false;
+    }
+
+    public void QuitGame () {
+        ConfirmMessage.SetActive(true);
+    }
+
+    public void ConfirmQuit () {
+
+    }
+
+    public void DeclineQuit () {
+
+    }
+}
