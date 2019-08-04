ab1ffa5cb1cc4e2f49ed82e92ec7b042a5a6a775
Matthew Morris <m.morris9491@gmail.com>
2019-08-04 23:59:30 +0200

    Fixed breaking platforms bug

--- Celeste Clone/Assets/Scripts/DevanScripts/BreakingPlatform.cs
+++ Celeste Clone/Assets/Scripts/DevanScripts/BreakingPlatform.cs
@@ -11,30 +11,49 @@ public class BreakingPlatform : MonoBehaviour
     private float time_to_break = 0.5f;
     [SerializeField]
     private float time_to_comeback = 2f;
-    
+
+    public BreakingPlatformsManager BPM;
     void Start()
     {
         rd_breakplat = GetComponent<Rigidbody2D>();
+        BPM = FindObjectOfType<BreakingPlatformsManager>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.name.Equals("Player"))
         {
-            Invoke("BreakPlatform", time_to_break);
-            Invoke("BringBackPlat", time_to_comeback);
+            Debug.Log(this.gameObject.name);
+            if (this.gameObject.name == "BreakingPlat")
+            {
+                Debug.LogWarning("we enter here");
+                BPM.DelayedDisable1();
+            }
+            else if (this.gameObject.name == "BreakingPlat (1)")
+            {
+                BPM.DelayedDisable2();
+            }
+            else
+            {
+                BPM.DelayedDisable3();
+            }
+            //Invoke("BreakPlatform", time_to_break);
+            //Invoke("BringBackPlat", time_to_comeback);
         }
     }
 
     void BreakPlatform()
     {
-        this.gameObject.GetComponent<Collider2D>().enabled = false;
+        Debug.LogError("we enter here");
+        this.gameObject.GetComponent<BoxCollider2D>().enabled = false;
         animator.SetBool("isActive", false);
+        Invoke("BringBackPlat", time_to_comeback);
     }
 
     void BringBackPlat()
     {
-        this.gameObject.GetComponent<Collider2D>().enabled = true;
+        Debug.LogError("we enter here 2");
+        this.gameObject.GetComponent<BoxCollider2D>().enabled = true;
         animator.SetBool("isActive", true);
     }
 }
---
+++ Celeste Clone/Assets/Scripts/DevanScripts/BreakingPlatformsManager.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakingPlatformsManager : MonoBehaviour
+{
+    public GameObject platform1;
+    public GameObject platform2;
+    public GameObject platform3;
+    public float spawnDelay;
+    public float breakDelay;
+    public float timer1;
+    public float timer2;
+    public float timer3;
+    public bool enabled1 = true;
+    public bool enabled2 = true;
+    public bool enabled3 = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!enabled1 && (Time.time >= timer1))
+        {
+            Enable1();
+
+        }
+        if (!enabled2 && (Time.time >= timer2))
+        {
+            Enable2();
+        }
+        if (!enabled3 && (Time.time >= timer3))
+        {
+            Enable3();
+
+        }
+    }
+
+    public void DelayedDisable1()
+    {
+        Invoke("Disable1", breakDelay);
+    }
+
+    public void DelayedDisable2()
+    {
+        Invoke("Disable2", breakDelay);
+    }
+
+    public void DelayedDisable3()
+    {
+        Invoke("Disable3", breakDelay);
+    }
+
+    public void Disable1()
+    {
+        platform1.SetActive(false);
+        enabled1 = false;
+        timer1 = Time.time + spawnDelay;
+    }
+
+    public void Disable2()
+    {
+        platform2.SetActive(false);
+        enabled2 = false;
+        timer2 = Time.time + spawnDelay;
+    }
+
+    public void Disable3()
+    {
+        platform3.SetActive(false);
+        enabled3 = false;
+        timer3 = Time.time + spawnDelay;
+    }
+
+    public void Enable1()
+    {
+        platform1.SetActive(true);
+        enabled1 = true;
+    }
+
+    public void Enable2()
+    {
+        platform2.SetActive(true);
+        enabled2 = true;
+    }
+
+    public void Enable3()
+    {
+        platform3.SetActive(true);
+        enabled3 = true;
+    }
+}
